889a71ae22ff741c91d1239be67e7b7ca346e940
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-25 08:33:40 +0900

    Initial Commit

---
+++ BottomMenu/BottomMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomMenuApp.swift
+//  BottomMenu
+//
+//  Created by 名前なし on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BottomMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
