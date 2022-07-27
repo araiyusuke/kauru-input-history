945636f7116c6a0a88c21a84fd6aad76d406179d
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-27 14:15:54 +0900

    move: 各丸ライブラリをファイルに移動

--- BottomMenu/ContentView.swift
+++ BottomMenu/ContentView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
 
+    enum FocusTextFields {
+        case input
+    }
+
     @StateObject private var viewModel: ViewModel = ViewModel()
     @ObservedObject var keyboard: KeyboardObserver = KeyboardObserver()
-
+    @FocusState var onFocus: FocusTextFields?
+    @State var inputText: String = ""
     let keyboardMenuHeight: CGFloat = 140
     let safeAreaHeight: CGFloat = 34
     let keyboardHeight: CGFloat = 336
+
+    var isShowBottomSheets: Bool {
+        return keyboard.isShowing
+    }
 
     var body: some View {
 
@@ -24,15 +33,22 @@ struct ContentView: View {
 
                 formList
 
-                inpuTextBttomSheet
-                    .frame(
-                        maxWidth: .infinity, maxHeight: 300,
-                                       alignment: .topTrailing)
-                    .background(RoundedCorners(color: Color.rgb(241, 241,243), tl: 20, tr: 20, bl: 0, br: 0))
+                VStack(spacing: 0) {
 
-
+                    inpuTextBttomSheet
+                        .frame(
+                            maxHeight: keyboardHeight + keyboardMenuHeight,                                           alignment: .topTrailing)
+                        .background(RoundedCorners(color: Color.rgb(241, 241,243), tl: 20, tr: 20, bl: 0, br: 0))
+                        .onTapGesture {
+                            onFocus = .input
+                        }
+                }
+                .opacity(isShowBottomSheets || inputText.isEmpty == false  ? 1 : 0)
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .background(Color.gray.opacity(keyboard.isShowing ? 0.3 : 0))
             }
             .ignoresSafeArea(.keyboard,  edges: keyboard.isShowing  ?  [.bottom] : [] )
+            .ignoresSafeArea(edges: [.bottom])  // 下端と右端を拡張
             .frame(maxHeight: .infinity)
             .background(Color.white)
         }
@@ -47,8 +63,11 @@ struct ContentView: View {
         VStack(spacing: 10) {
             List {
                 ForEach(1..<25, id: \.self) { index in
-                    titleTextFeild
+                    Text("入力テスト")
                         .frame(maxWidth: .infinity, maxHeight: 30)
+                        .onTapGesture {
+                            self.onFocus = .input
+                        }
                 }
             }
             // リストのフォーマットをリセット
@@ -66,9 +85,12 @@ struct ContentView: View {
                 .font(.headline)
                 .foregroundColor(.black)
                 .frame(maxWidth: .infinity, maxHeight: 30, alignment: .leading)
-
-            TextField("駅名を入力して下さい", text: Binding.constant(""))
+                .onTapGesture {
+                    UIApplication.shared.closeKeyboard()
+                }
+            TextField("駅名を入力して下さい", text: $inputText)
                 .foregroundColor(Color.black)
+                .focused($onFocus, equals: .input)
                 .padding(.leading, 5)
                 .frame(maxWidth: .infinity, maxHeight: 40, alignment: .leading)
                 .background(Color.white)
@@ -76,9 +98,28 @@ struct ContentView: View {
                     RoundedRectangle(cornerRadius: 4)
                         .stroke(Color.blue, lineWidth: 1)
                 )
+
+            if (inputText.count == 5 || keyboard.isShowing == false) {
+                VStack(spacing: 10) {
+                    VStack {
+                        ForEach(1..<10, id: \.self) { index in
+                            Text("+")
+                                .frame(maxWidth: .infinity, maxHeight: 30)
+                                .onTapGesture {
+                                    self.onFocus = .input
+                                }
+                        }
+                    }
+                    .frame(height: 100, alignment: .top)
+                    // リストのフォーマットをリセット
+                    .listStyle(.plain)
+
+                }
+
+            }
         }
         .padding()
-        .frame(maxWidth: .infinity, maxHeight: keyboardMenuHeight)
+//        .frame(maxWidth: .infinity, maxHeight: keyboardMenuHeight)
     }
 
     var titleTextFeild: some View {
@@ -147,37 +188,3 @@ class KeyboardObserver: ObservableObject {
 
 
 
-struct RoundedCorners: View {
-    var color: Color = .blue
-    var tl: CGFloat = 0.0
-    var tr: CGFloat = 0.0
-    var bl: CGFloat = 0.0
-    var br: CGFloat = 0.0
-
-    var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-
-                let w = geometry.size.width
-                let h = geometry.size.height
-
-                // Make sure we do not exceed the size of the rectangle
-                let tr = min(min(self.tr, h/2), w/2)
-                let tl = min(min(self.tl, h/2), w/2)
-                let bl = min(min(self.bl, h/2), w/2)
-                let br = min(min(self.br, h/2), w/2)
-
-                path.move(to: CGPoint(x: w / 2.0, y: 0))
-                path.addLine(to: CGPoint(x: w - tr, y: 0))
-                path.addArc(center: CGPoint(x: w - tr, y: tr), radius: tr, startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
-                path.addLine(to: CGPoint(x: w, y: h - br))
-                path.addArc(center: CGPoint(x: w - br, y: h - br), radius: br, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
-                path.addLine(to: CGPoint(x: bl, y: h))
-                path.addArc(center: CGPoint(x: bl, y: h - bl), radius: bl, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
-                path.addLine(to: CGPoint(x: 0, y: tl))
-                path.addArc(center: CGPoint(x: tl, y: tl), radius: tl, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
-            }
-            .fill(self.color)
-        }
-    }
-}
